6bbe77f19d8f7cd3df5c1c40405809559d3ade88
Fabs <lartin.fabien@gmail.com>
2024-02-29 15:42:25 +0100

    feat add ul html element

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,21 @@ function App() {
   return (
     <>
       <h1>fabien lartin</h1>
-      <li>
-        <a href="https://twitter.com/wolfmefive">x, formerly twitter</a>
-      </li>
-      <li>
-        <a href="https://github.com/lf444">github</a>
-      </li>
-      <li>
-        <a href="https://www.linkedin.com/in/fabien-lartin">linkedin</a>
-      </li>
-      <li>
-        <a href="">project showcase</a>
-      </li>
-      <li>lartin.fabien@gmail.com </li>
+      <ul>
+        <li>
+          <a href="https://twitter.com/wolfmefive">x, formerly twitter</a>
+        </li>
+        <li>
+          <a href="https://github.com/lf444">github</a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/fabien-lartin">linkedin</a>
+        </li>
+        <li>
+          <a href="">project showcase</a>
+        </li>
+        <li>lartin.fabien@gmail.com </li>
+      </ul>
     </>
   );
 }
