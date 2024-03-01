e4d920780978bb3d0e39af20ba2e9b8336bfa8b6
Fabs <lartin.fabien@gmail.com>
2024-03-01 01:06:55 +0100

    refactor try hashrooter for github pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { RouterProvider, createHashRouter } from "react-router-dom";
 import ErrorPage from "./error-page.tsx";
 import Cv from "./pages/Cv";
 import "./index.css";
 import "./i18n.ts";
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <App />,
